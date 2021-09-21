1fe3e0ffc54f990a8ca10b2048a55b048a0c74df
Hector David Madrid R <hector.morelos.607@gmail.com>
2021-09-20 21:26:28 -0500

    New generator modules created and new unit tests

--- lib/game_number_generator.dart
+++ lib/game_number_generator.dart
@@ -1,17 +1,54 @@
 import 'dart:math';
 
-List<int> numberGenerator() {
+List<int> numberGeneratorBase(int maxRange, int numberOfElements) {
   List<int> numbers = [];
   Random randomGenerator = Random();
 
-  for (int i = 0; i < 30; i++) {
-    int randomNumber = randomGenerator.nextInt(60);
+  for (int i = 0; i < numberOfElements; i++) {
+    int randomNumber = randomGenerator.nextInt(maxRange);
 
     while (numbers.contains(randomNumber)) {
-      randomNumber = randomGenerator.nextInt(60);
+      randomNumber = randomGenerator.nextInt(maxRange);
     }
 
     numbers.add(randomNumber);
   }
   return numbers;
+}
+
+List<int> generatorEasyMode() {
+  int maxRange = 60;
+  int numberOfElements = 30;
+
+  List<int> numbersEasyMode = numberGeneratorBase(maxRange, numberOfElements);
+  return numbersEasyMode;
+}
+
+List<int> generatorNormalMode() {
+  int maxRange = 90;
+  int numberOfElements = 45;
+
+  List<int> numberNormalMode = numberGeneratorBase(maxRange, numberOfElements);
+  return numberNormalMode;
+}
+
+List<int> generatorHardMode() {
+  int maxRange = 120;
+  int numberOfElements = 45;
+
+  List<int> numberNormalMode = numberGeneratorBase(maxRange, numberOfElements);
+  return numberNormalMode;
+}
+
+List<String> signsGeneratorBase(int numberElements) {
+  List<String> signsCollection = ['+', '-', '*', '/'];
+  List<String> signs = [];
+  Random randomGenerator = Random();
+
+  for (int i = 0; i < numberElements; i++) {
+    int randomSign = randomGenerator.nextInt(4);
+    signs.add(signsCollection.elementAt(randomSign));
+  }
+
+  return signs;
 }
--- test/game_number_generator_test.dart
+++ test/game_number_generator_test.dart
@@ -2,7 +2,23 @@ import 'package:game_math/game_number_generator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('generator', () {
-    expect(numberGenerator().length, 30);
+  test('numberGeneratorBaseTest', () {
+    expect(numberGeneratorBase(60, 30).length, 30);
+  });
+
+  test('generatorEasyModeTest', () {
+    expect(generatorEasyMode().length, 30);
+  });
+
+  test('generatorNormalModeTest', () {
+    expect(generatorNormalMode().length, 45);
+  });
+
+  test('generatorHardModeTest', () {
+    expect(generatorHardMode().length, 45);
+  });
+
+  test('singsGeneratorBaseTest', () {
+    expect(signsGeneratorBase(15).length, 15);
   });
 }
